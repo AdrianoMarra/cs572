851e88549276a69c590fa98658823b5510a0f5a5
Adriano Marra <adrianomarra90@gmail.com>
2019-05-27 20:42:36 -0500

    Update submition for Lab 01

--- lab01/lab01.js
+++ lab01/lab01.js
@@ -1,44 +1,102 @@
-// const removeNum = function (num) {
-//     const arr = this;
-//     return new Promise((resolve, reject) => {
-//         setTimeout(function() {
-//             resolve(arr.filter(item => item != num)) 
-//         }, 1000);
-//     });
-// }
+/**
+ * Resp question 01:
+ */
 
-// Array.prototype.removeNum = removeNum;
+const removeNum = function (num) {
+    const arr = this;
+    return new Promise((resolve, reject) => {
+        resolve(arr.filter(item => item != num))
+    });
+}
 
-// console.log("Start");
-// [1,3,4,2,1,5].removeNum(1).then(response => console.log(response))
-// console.log("Finish");
+Array.prototype.removeNum = removeNum;
 
+console.log("Start");
+[1,3,4,2,1,5].removeNum(1).then(response => console.log(response))
+console.log("Finish");
 
-const fetchApi = function () {
+/**
+ * Resp question 02:
+ */
 
-    console.log("before fetchApi");
+var URL = 'https://randomuser.me/api/';
 
-    fetch('https://randomuser.me/api/')
-    .then(response => response.json())
-    .then(myJson => {
-        console.log(myJson.results[0].name)
-        console.log(myJson.results[0].location)
+const formatData = function (results) {
+    let resp = [];
+
+    results.forEach(element => {
+        const obj = { name, location };
+        obj.name = element.name;
+        obj.location = element.location;
+        resp.push(obj);
     });
 
-    console.log("after fetchApi");
+    return resp;
+}
 
+function simpleFetchApi() {
+    console.log("Before simpleFetchApi");
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(myJson => {
+            console.log(formatData(myJson.results));
+        });
+
+    console.log("After simpleFetchApi");
+}
+
+const fetchApiPromise = function () {
+    return new Promise((resolve, reject) => {
+
+        resolve(
+            fetch(URL)
+                .then(response => response.json())
+                .then(myJson => {
+                    return formatData(myJson.results);
+                })
+        );
+
+        reject(new Error("Call failled"));
+    });
 }
 
 async function asyncAwaitFetch() {
-
-    console.log("before asyncAwaitFetch");
-
+    console.log("Before asyncAwaitFetch");
     try {
-        let results = await fetchApi();
+        const results = await fetchApiPromise();
+        console.log("Keep doing something...");
+        console.log(results);
     } catch (error) {
         console.log(error);
     }
-
-    console.log("after asyncAwaitFetch");
-
+    console.log("After asyncAwaitFetch");
 }
+
+const { from } = rxjs;
+const { map } = rxjs.operators;
+
+const reactiveFetch = function () {
+    console.log("Before reactiveFetch");
+
+    let myPromise = fetchApiPromise();
+    let obs$ = from(myPromise);
+
+    obs$.pipe(
+        // I am using map here just to show how 
+        // we could use the operators inside the pipe 
+        map((item, idx) => {
+            return {
+                mappedName: item[idx].name, 
+                mappedLocation: item[idx].location
+            }
+        })
+    )
+        .subscribe((item) => console.log(item));
+
+    console.log("After reactiveFetch");
+}
+
+/**
+ * All the methods above are Asyncronous.
+ * */
