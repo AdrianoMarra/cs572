62691b6a45c195aaa2cb9a2c3f0129849b8adeb8
Adriano Marra <adrianomarra90@gmail.com>
2019-05-30 17:10:21 -0500

    Submit lab04

--- lab04/script.js
+++ lab04/script.js
@@ -1,5 +1,32 @@
 const {Subject} = require('rxjs');
 const subject = new Subject();
+const http = require("http");
+const {fork} = require('child_process');
+const url = require('url');
 
-console.log("test");
-console.log("awesome");
+
+function readFiles({req, res}) {
+    const childProcess = fork('childProcess.js');
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    
+    childProcess.send(query.url);
+    childProcess.on('message', resp => {
+        res.write(resp.toString());
+    })
+
+    childProcess.on('exit', () => {
+        res.end();
+    });
+};
+
+subject.subscribe(readFiles);
+
+http.createServer((req, res) => {
+    
+    if (req.url == '/favicon.ico') 
+        res.end(); 
+    else
+        subject.next({ req: req, res: res })
+
+}).listen(4000, () => console.log("Main process listening on 4000 port"));
