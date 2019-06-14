eb2b01f764b2829f0ef870f85efdf9b9cd6c13da
Adriano Marra <adrianomarra90@gmail.com>
2019-06-13 23:29:12 -0500

    Submition of homework_13

---
+++ homework_13/my-new-app/src/app/services/getdata.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+
+
+@Injectable({providedIn: 'root'})
+export class GetDataService {
+
+    constructor(public http: HttpClient) {}
+
+    getOnlineData() {
+        return this.http.get('https://randomuser.me/api/?results=10');
+    }
+
+    getCachedData() {
+        let storageData = JSON.parse(localStorage.onlineData).results;
+        return from(storageData).pipe(shareReplay());
+    }
+}
